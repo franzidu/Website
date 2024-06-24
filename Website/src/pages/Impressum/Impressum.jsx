89081624463de0592068bd432611ab2632b8070f
franzidu <stfrdudi@stud.h-da.de>
2024-06-24 15:56:56 +0200

    typo correct

--- Website/src/pages/Impressum/Impressum.jsx
+++ Website/src/pages/Impressum/Impressum.jsx
@@ -7,7 +7,7 @@ function Impressum() {
     <div>
         <Navbar/>
         <h1>Impressum</h1>
-        <p>Aufgrand von privater Nutzung und Bereitstellung dieser Webseite sind die Kontaktdaten der verantwortlichen Person vorbehalten.</p>
+        <p>Aufgrund von privater Nutzung und Bereitstellung dieser Webseite und allen verbundenen Subseiten sind die Kontaktdaten der verantwortlichen Person vorbehalten.</p>
 
         <h1>Datenschutzerklärung</h1>
         <p>Die Datenschutzerklärung befindet sich in Bearbeitung.</p>
