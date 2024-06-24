10e7eb25b678ed8b0f60586d97d14dbb31119d41
franzidu <stfrdudi@stud.h-da.de>
2024-06-24 15:55:56 +0200

    update Impressum

--- Website/src/pages/Impressum/Impressum.jsx
+++ Website/src/pages/Impressum/Impressum.jsx
@@ -7,12 +7,11 @@ function Impressum() {
     <div>
         <Navbar/>
         <h1>Impressum</h1>
-        <p>Franziska Dudichum</p>
-        <p>Gartenstraße 1</p>
-        <p>34628 Willingshausen</p>
+        <p>Aufgrand von privater Nutzung und Bereitstellung dieser Webseite sind die Kontaktdaten der verantwortlichen Person vorbehalten.</p>
 
-        <h1>Kontakt</h1>
-        <p><b>Email:</b> f.dudichum@gmx.de</p>
+        <h1>Datenschutzerklärung</h1>
+        <p>Die Datenschutzerklärung befindet sich in Bearbeitung.</p>
+        <p>Für die Inhalte und Richtigkeit von verlinkten Webseiten ist der Betreiber dieser Webseite nicht verantwortlich.</p>
     </div> );
 }
 
