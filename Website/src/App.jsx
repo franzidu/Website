b7a3b6d0dfb31a6ed186d67180df9915832d2fc8
franzidu <stfrdudi@stud.h-da.de>
2024-06-24 13:42:23 +0200

    fist approaches to website

---
+++ Website/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.scss'
+import Navbar from "./components/Navbar/Navbar.jsx"
+
+function App() {
+
+  return (
+    <div>
+      <Navbar/>
+      <h1>Test Header</h1>
+      <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</p>
+      <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet </p>
+      <a href="http://youtube.de" target='_blank'>Youtube</a>
+    </div>
+  )
+}
+
+export default App
---
+++ Website/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from "./Navbar.module.scss"
+import Logo from "../../assets/images/moonstar.svg"
+
+function Navbar() {
+    return ( 
+        <div>
+            <div className={styles.head}>
+                    <img className={styles.logo} src={Logo} alt="Logo" />
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href="/">Home</a>
+                                <a href="page1">page1</a>
+                                <a href="page2">page2</a>
+                            </li>
+                        </ul>
+                    </nav>
+                 </div>
+            </div>
+        
+     );
+}
+
+export default Navbar;
