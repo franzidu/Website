7b1c1c0d4f642ead00b96e2c79d839ac4fbc1aa2
franzidu <stfrdudi@stud.h-da.de>
2024-06-24 15:11:44 +0200

    added content to subpages

--- Website/src/pages/page1/page1.jsx
+++ Website/src/pages/page1/page1.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import Navbar from '../../components/Navbar/Navbar.jsx';
+import Footer from '../../components/Footer/Footer.jsx';
+
+
+
 function page1() {
-    return ( <div></div> );
+    return ( 
+    <div>
+        <Navbar/>
+        <p>This is another subsite of my website</p>
+        <Footer/>
+    </div> );
 }
 
 export default page1;
--- Website/src/pages/page2/page2.jsx
+++ Website/src/pages/page2/page2.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Navbar from '../../components/Navbar/Navbar.jsx';
+import Footer from '../../components/Footer/Footer.jsx';
+
+
 
 function page2() {
-    return ( <div></div> );
+    return ( 
+    <div>
+        <Navbar/>
+        <p>This is another another subsite of my website</p>
+        <Footer/>
+    </div> );
 }
 
 export default page2;
